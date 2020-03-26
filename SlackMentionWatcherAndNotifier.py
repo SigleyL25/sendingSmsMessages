2a46a81c44d943d944dc89bdd3710fd4f54c8559
Larry Sigley <SigleyL25@gmail.com>
2020-03-26 16:44:14 -0700

    Defined software requirements.

---
+++ SlackMentionWatcherAndNotifier.py
@@ -0,0 +1,7 @@
+# By: Larry Sigley
+
+# Get Slack Credentials
+# Get Users cell phone number
+# Store cedentials and cell number (user app data directory)
+# Check Slack application for messages or mentions
+# Send SMS message to cell phone if messaged or mentioned
